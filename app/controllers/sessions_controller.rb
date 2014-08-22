8a7dbab35f83144f9854dfa0baaf2d1f464e7625
rails <rails@rails.(none)>
2014-08-22 17:14:09 +0530

    first commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < Devise::SessionsController
+
+  def new
+    @users = User.all
+  end
+
+  def create
+    @user = User.find_by_email_and_role(params[:user][:email],params[:user][:role])
+    respond_to do |format|
+      format.js
+    end
+  end
+
+
+end

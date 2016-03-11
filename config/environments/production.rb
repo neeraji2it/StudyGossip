d2f4222bf57e73913e701264a127a7fd11db66e4
rails bhagath <rails@gmail1.com>
2016-03-11 11:42:45 +0530

    testing of password reset in server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ WebApp::Application.configure do
 
     # Send deprecation notices to registered listeners
     config.active_support.deprecation = :notify
-    config.action_mailer.default_url_options = { :host => 'http://studygossip.com' }
+    config.action_mailer.default_url_options = { :host => 'studygossip.com' }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
         :address              => "smtp.gmail.com",
